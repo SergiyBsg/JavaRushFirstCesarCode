2147bcbf0bc88cc17047ea8f44b79143333c24de
Sergiy Bloshchenko <sergiy.bloshchenko@nentgroup.com>
2023-05-24 08:57:24 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
-        String qw = new FileService("C:\\Users\\sergey.bloschenko\\Projects\\JavaRush\\JavaRushFirstCesarCode\\ToPlantGarden.txt").readFile();
-        System.out.println(qw);
-        String path = new File("ToPlantGarden.txt").getAbsolutePath();
-        System.out.println(path);
+        System.out.println("Hello world!");
     }
 }
